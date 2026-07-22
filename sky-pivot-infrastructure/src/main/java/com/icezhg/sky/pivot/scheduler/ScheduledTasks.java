801655c8d4870eb25ee00b860bafe0ab142dd282
zhongjibing <zhongjibing@163.com>
2026-07-23 00:39:37 +0800

    refactor: update ScheduledTasks to use VaultItemRepository
    - Replace PasswordRepository with VaultItemRepository for trash cleanup
    - Keep LoginHistory cleanup scheduling unchanged

--- sky-pivot-infrastructure/src/main/java/com/icezhg/sky/pivot/scheduler/ScheduledTasks.java
+++ sky-pivot-infrastructure/src/main/java/com/icezhg/sky/pivot/scheduler/ScheduledTasks.java
@@ -3,7 +3,7 @@ package com.icezhg.sky.pivot.scheduler;
 import com.icezhg.sky.pivot.config.properties.LoginHistoryProperties;
 import com.icezhg.sky.pivot.config.properties.TrashProperties;
 import com.icezhg.sky.pivot.repository.LoginHistoryRepository;
-import com.icezhg.sky.pivot.repository.PasswordRepository;
+import com.icezhg.sky.pivot.repository.VaultItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,16 +17,16 @@ public class ScheduledTasks {
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    private final PasswordRepository passwordRepository;
+    private final VaultItemRepository vaultItemRepository;
     private final LoginHistoryRepository loginHistoryRepository;
     private final int trashRetentionDays;
     private final int loginHistoryRetentionMonths;
 
-    public ScheduledTasks(PasswordRepository passwordRepository,
+    public ScheduledTasks(VaultItemRepository vaultItemRepository,
                           LoginHistoryRepository loginHistoryRepository,
                           TrashProperties trashProperties,
                           LoginHistoryProperties loginHistoryProperties) {
-        this.passwordRepository = passwordRepository;
+        this.vaultItemRepository = vaultItemRepository;
         this.loginHistoryRepository = loginHistoryRepository;
         this.trashRetentionDays = trashProperties.getRetentionDays();
         this.loginHistoryRetentionMonths = loginHistoryProperties.getRetentionMonths();
@@ -36,7 +36,7 @@ public class ScheduledTasks {
     @Transactional
     public void cleanExpiredTrash() {
         LocalDateTime threshold = LocalDateTime.now().minusDays(trashRetentionDays);
-        int deleted = passwordRepository.deleteByDeletedAtBefore(threshold);
+        int deleted = vaultItemRepository.deletePermanentlyByDeletedAtBefore(threshold);
         log.info("Cleaned {} expired trash entries", deleted);
     }
 
